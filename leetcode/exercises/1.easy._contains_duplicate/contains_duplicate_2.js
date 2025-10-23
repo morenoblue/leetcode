0ee29e08883616b7dc93f2195300b8cd53e24f7f
morenoblue <morenoblue2000@gmail.com>
2025-10-23 07:45:19 +0100

    solved contains duplicate in js

--- leetcode/exercises/1.easy._contains_duplicate/contains_duplicate_2.js
+++ leetcode/exercises/1.easy._contains_duplicate/contains_duplicate_2.js
@@ -10,14 +10,16 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    const counts = {};
-    for (const n of nums){
-        counts[n] = (counts[n] ?? 0) + 1;
-        if (counts[n] > 1){ return true; };
+var containsDuplicate = function(nums) 
+{
+    nums.sort();
+
+    for (let i = 1; i < nums.length; i++)
+    {
+        if (nums[i-1] === nums[i]){ return true };
     };
     return false
 };
 
-console.log(containsDuplicate([1, 2, 3, 10, 8]))
+console.log(containsDuplicate([1, 2, 3, 10, 8, 1]))
 
