4d5e18d1918d314c8208cd2dc283ef143aed4925
morenoblue <morenoblue2000@gmail.com>
2025-10-24 09:28:23 +0100

    add two_sum_1.js

--- leetcode/exercises/2.easy.two_sum/two_sum_1.js
+++ leetcode/exercises/2.easy.two_sum/two_sum_1.js
@@ -1,1 +1,24 @@
-// Self Solved: ?
+// -----------------------
+// Created       : 24/10/2025
+// Last Edited   : 24/10/2025
+// Self Solved   : False
+// Big O         : O(n)
+// Topics        : 
+// -----------------------
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = {};
+    for (const [i, value] of nums.entries())
+    {
+        const complement = target - value;
+        if (complement in map) { return [map[complement], i] };
+        map[value] = i;
+    };
+};
+
+console.log(twoSum([1, 2, 3, 10, 4, 8, 7], 12))
