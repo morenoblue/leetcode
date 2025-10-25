5abd5e9d6d7c55a81ddf047876acaef3946f4951
morenoblue <morenoblue2000@gmail.com>
2025-10-25 23:05:25 +0100

    add port group anagram solution in js

--- leetcode/exercises/4.medium.group_anagrams/group_anagrams_2.js
+++ leetcode/exercises/4.medium.group_anagrams/group_anagrams_2.js
@@ -1,9 +1,27 @@
 // -----------------------
-// Created       : 
-// Last Edited   : 
+// Created       : 25/10/2025
+// Last Edited   : 25/10/2025
 // Big O         :
 // Topics        : 
 // Problem Id    : 49
 // -----------------------
 
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    map = {}; 
+    for (const str of strs)
+    {
+        const key = Array(26).fill(0);
+        for (const char of str)
+        {
+            key[char.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+        };
+        (map[key] ?? (map[key] = [])).push(str); 
+    };
+    return Object.values(map);
+};
 
+console.log(groupAnagrams(["oi", "tchau", "io", "tui", "itu"]));
