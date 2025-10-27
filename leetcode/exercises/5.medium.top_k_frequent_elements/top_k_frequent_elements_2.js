fae6a89b7a3943a2f24a4fe4e6adb33ad1c9cd02
morenoblue <morenoblue2000@gmail.com>
2025-10-27 22:55:01 +0000

    add new js solution

--- leetcode/exercises/5.medium.top_k_frequent_elements/top_k_frequent_elements_2.js
+++ leetcode/exercises/5.medium.top_k_frequent_elements/top_k_frequent_elements_2.js
@@ -1,7 +1,41 @@
 // -----------------------
-// Created       : 
-// Last Edited   : 
+// Created       : 27/10/2025
+// Last Edited   : 27/10/2025
 // Big O         :
 // Topics        : 
 // Problem Id    : 347
 // -----------------------
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const counts = {}
+    for (num of nums)
+    {
+        counts[num] = (counts[num] ?? 0) + 1;
+    };
+
+    const frequency = Array.from({length: nums.length}, () => []);
+    for (num of Object.keys(counts))
+    {
+        frequency[counts[num]-1].push(Number(num));
+    };
+
+    res = []
+    for (let i = frequency.length-1; i >= 0; i--)
+    {
+        for (const j of frequency[i])
+        {
+            if (res.length !== k){
+                res.push(j);
+            };
+        };
+    };
+
+    return res;
+};
+
+console.log(topKFrequent([1,1,2,2,3,3,3,5,6,5], 3));
