817132b0a21143919185b9911c71e8428a77b401
morenoblue <morenoblue2000@gmail.com>
2025-11-02 17:53:47 +0000

    add GO solutiongit add .!

---
+++ leetcode/exercises/4.medium.group_anagrams/group_anagrams_2.go
@@ -0,0 +1,37 @@
+// -----------------------
+// Created       : 02/11/2025
+// Last Edited   : 02/11/2025
+// Big O         :
+// Topics        : 
+// Problem Id    : 49
+// -----------------------
+
+package main
+
+import (
+    "fmt"
+    "maps"
+    "slices"
+)
+
+func groupAnagrams(strs []string) [][]string {
+
+    m := make(map[[26]int][]string) 
+
+    for _, v := range strs {
+
+        key := [26]int{}
+        for i := 0; i < len(v); i++ {
+            key[v[i] - 'a'] = key[v[i] - 'a'] + 1
+        }
+        m[key] = append(m[key], v)
+    }
+
+    vals := maps.Values(m)
+
+    return slices.Collect(vals)
+}
+
+func main() {
+    fmt.Println(groupAnagrams([]string{"eat","tea","tan","ate","nat","bat"}))
+}
