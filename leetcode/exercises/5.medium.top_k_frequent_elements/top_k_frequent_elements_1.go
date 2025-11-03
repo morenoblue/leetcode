9904b83c03a360b31070985f4187e415fca5e5be
morenoblue <morenoblue2000@gmail.com>
2025-11-03 11:24:22 +0000

    GOGOule

---
+++ leetcode/exercises/5.medium.top_k_frequent_elements/top_k_frequent_elements_1.go
@@ -0,0 +1,44 @@
+// -----------------------
+// Created       : 03/11/2025
+// Last Edited   : 03/11/2025
+// Big O         :
+// Topics        : 
+// Problem Id    : 347
+// -----------------------
+
+package main
+
+import (
+    "fmt"
+)
+
+func topKFrequent(nums []int, k int) []int {
+
+    counts := make(map[int]int)
+    for _,v := range nums {
+        if _, ok := counts[v]; !ok{
+        counts[v] = -1
+        }
+        counts[v]++
+    }
+
+    freq := make([][]int, len(nums))
+    for k,v := range counts {
+        freq[v] = append(freq[v], k)
+    }
+
+    result := []int{}
+    for i := len(freq) - 1; i >= 0; i-- {
+        for _, v := range freq[i] {
+            result = append(result, v)
+            if len(result) == k { return result }
+        }
+    }
+
+    return result
+    
+}
+
+func main() {
+    fmt.Println(topKFrequent([]int{1, 1, 3, 1, 3, 2, 10, 10, 9}, 3))
+}
---
+++ leetcode/exercises/6.medium.encode_decode_strings/encode_decode_strings_3.go
@@ -0,0 +1,56 @@
+// -----------------------
+// Created       : 03/11/2025
+// Last Edited   : 03/11/2025 
+// Big O         :
+// Topics        : 
+// Problem Id    : 271
+// -----------------------
+
+package main
+
+import (
+    "fmt"
+    "strconv"
+)
+
+type Solution struct{}
+
+func (so *Solution) Encode(strs []string) string {
+    encoded := string("")
+    for _, v := range strs {
+        encoded += fmt.Sprint(len(v)) + "#" + v
+    }
+    return encoded
+}
+
+func (so *Solution) Decode(s string) []string {
+    decoded := []string{}
+    i := 0
+    for i < len(s) {
+
+        current_length := ""
+        for s[i] != '#' {
+            current_length += string(s[i])
+            i += 1
+        }
+
+        n, err := strconv.Atoi(current_length)
+        if err != nil {
+            panic(err)
+        }
+
+        decoded = append(decoded, s[i+1:i+1+n])
+
+        i = i+1+n
+    }
+    return decoded
+}
+
+func main() {
+
+    s := Solution{}
+    fmt.Println(s.Encode([]string{"toma", "sai", "ova"}))
+    fmt.Println(s.Decode(s.Encode([]string{"toma", "sai", "ova"})))
+
+}
+
