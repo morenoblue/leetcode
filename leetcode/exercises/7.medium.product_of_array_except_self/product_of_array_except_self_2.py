e0e6a171bcd3c7ee8d9cd12a01b30affbe9867c5
morenoblue <morenoblue2000@gmail.com>
2025-11-08 06:00:17 +0000

    fix naming

--- leetcode/exercises/7.medium.product_of_array_except_self/product_of_array_except_self_2.py
+++ leetcode/exercises/7.medium.product_of_array_except_self/product_of_array_except_self_2.py
@@ -1,6 +1,6 @@
 # -----------------------
 # Created    : 05/11/2025
-# Last Edited: 05/11/2025 
+# Last Edited: 07/11/2025 
 # Topics     : 
 # Big O      :
 # Problem Id : 238
@@ -10,15 +10,15 @@ class Solution:
 
     def productExceptSelf(self, nums: list[int]) -> list[int]:
         res = [1]
-        postfix = 1
-        for i in range(len(nums)-1):
-            res.append(postfix*nums[i]) 
-            postfix *= nums[i] 
-
         prefix = 1
-        for i in range(-1, -len(nums), -1):
-            res[i-1] *= prefix*nums[i]
+        for i in range(len(nums)-1):
+            res.append(prefix*nums[i]) 
             prefix *= nums[i] 
+
+        postfix = 1
+        for i in range(-1, -len(nums), -1):
+            res[i-1] *= postfix*nums[i]
+            postfix *= nums[i] 
         return res
 
 s = Solution()
