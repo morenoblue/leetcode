9366503e55ded42a79149beeb5707f2a52bf2c94
morenoblue <morenoblue2000@gmail.com>
2025-11-12 18:05:51 +0000

    add new solution

---
+++ leetcode/exercises/2.0.easy.valid_palindrome/valid_palindrome_1.py
@@ -0,0 +1,41 @@
+# -----------------------
+# Created    : 12/11/2025
+# Last Edited: 12/11/2025 
+# Topics     : 
+# Big O      :
+# Problem Id : 125
+# -----------------------
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 1 and s.isalpha(): return True
+
+        left = 0
+        right = len(s) - 1
+        
+        while left < right:
+
+            while not s[left].isalnum():
+                left += 1 
+                if left == len(s): return True
+
+            while not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower(): 
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
+
+s = Solution()
+print(s.isPalindrome("A man, a plan, a canal: Panama"))
+print(s.isPalindrome(".,"))
+
+
+
+
+        
