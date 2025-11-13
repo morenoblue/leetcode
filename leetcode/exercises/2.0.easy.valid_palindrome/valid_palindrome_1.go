dee8dc460adc9975d0e40f8a65ed21df8f63647c
morenoblue <morenoblue2000@gmail.com>
2025-11-13 06:41:25 +0000

    add JS and GO sols to valid palindrome

---
+++ leetcode/exercises/2.0.easy.valid_palindrome/valid_palindrome_1.go
@@ -0,0 +1,52 @@
+// -----------------------
+// Created    : 13/11/2025
+// Last Edited: 13/11/2025 
+// Topics     : 
+// Big O      :
+// Problem Id : 125
+// -----------------------
+
+package main
+
+import (
+    "fmt"
+)
+
+func isPalindrome(s string) bool {
+
+    isalnum := func(c byte) bool {
+        return (97 <= c && c <= 122) ||
+               (65 <= c && c <= 90) ||
+               (48 <= c && c <= 57)
+    }
+
+    bytetolower := func(c byte) byte {
+        if 65 <= c && c <= 90 { 
+            return c + 97 - 65
+        } else {
+            return c
+        }
+    }
+
+    l := 0;
+    r := len(s) - 1;
+    for l < r {
+        for !isalnum(s[l]) {
+            l++
+            if l == len(s) { return true }
+        }
+        for !isalnum(s[r]) {
+            r--
+        }
+
+        if bytetolower(s[r]) != bytetolower(s[l]) { return false }
+        l++
+        r--
+    }
+    return true
+}
+
+func main() {
+    fmt.Println(isPalindrome("A man, a plan, a canal: Panama"))
+}
+
---
+++ leetcode/exercises/2.0.easy.valid_palindrome/valid_palindrome_1.js
@@ -0,0 +1,45 @@
+// -----------------------
+// Created    : 13/11/2025
+// Last Edited: 13/11/2025 
+// Topics     : 
+// Big O      :
+// Problem Id : 125
+// -----------------------
+
+/*
+* @param {string} s
+* @return {boolean}
+*/
+var isPalindrome = function (s) {
+
+    // function isalnum(s) {
+    //     return /^[A-Za-z0-9]+$/.test(s)
+    // }
+
+    function isalnum(c) {
+        const code = c.charCodeAt()
+        return (
+            (97 <= code && code <= 122) ||
+            (65 <= code && code <= 90) ||
+            (48 <= code && code <= 57)
+        )
+    }
+
+    l = 0;
+    r = s.length - 1;
+    while (l < r) {
+        while (!isalnum(s[l])) {
+            l++;
+            if (l == s.length) { return true }
+        }
+        while (!isalnum(s[r])) {
+            r--;
+        }
+        if (s[l].toLowerCase() != s[r].toLowerCase()) { return false }
+        l++;
+        r--;
+    }
+    return true
+}
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"))
