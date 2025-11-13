4212ebd165a04bc1fb93e5647208180a883c103a
morenoblue <morenoblue2000@gmail.com>
2025-11-13 19:16:18 +0000

    add go solution

---
+++ leetcode/exercises/2.3.medium.container_with_most_water/container_with_most_water.go
@@ -0,0 +1,40 @@
+// -----------------------
+// Created    : 13/11/2025
+// Last Edited: 13/11/2025 
+// Topics     : 
+// Big O      :
+// Problem Id : 11
+// -----------------------
+
+package main
+
+import (
+    "fmt"
+)
+
+func maxArea(height []int) int {
+    l := 0
+    r := len(height) - 1
+    max_height := 0
+    base := 0
+    h := 0
+    for i := 0; i < len(height); i++ {
+        base = max((r - l), -(r - l))
+        h = min(height[l], height[r])
+        max_height = max(max_height, base*h)
+        if height[l] < height[r] {
+            l++
+        } else if height[l] > height[r] {
+            r--
+        } else {
+            l++
+            r--
+        }
+    }
+    return max_height
+}
+
+func main() {
+    fmt.Print(maxArea([]int{1,2,1}));
+}
+
