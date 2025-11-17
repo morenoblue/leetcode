a120cdfa2a9dcb7badb1b736fa52cfdb2975f44e
morenoblue <morenoblue2000@gmail.com>
2025-11-17 07:01:24 +0000

    add new python solution

---
+++ leetcode/exercises/2.2.1.easy.valid_parenthesis.py
@@ -0,0 +1,31 @@
+# -----------------------
+# Created    : 17/11/2025
+# Last Edited: 17/11/2025 
+# Topics     : 
+# Big O      :
+# Problem Id : 20
+# -----------------------
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        map = {
+                "}": "{",
+                ")": "(",
+                "]": "[",
+        }
+        stack = []
+        for i in s:
+            if i in map:
+                if len(stack) == 0 or stack[-1] != map.get(i, False):
+                    return False
+                stack.pop()
+            else:
+                stack.append(i)
+
+        return True if len(stack) == 0 else False 
+        
+
+s = Solution()
+print(s.isValid("(){}}{"))
+
+  
