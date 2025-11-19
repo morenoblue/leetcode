469376495a3ce09717cd2bc198c727b769850b11
morenoblue <morenoblue2000@gmail.com>
2025-11-19 08:42:58 +0000

    add python sol to new ex

---
+++ leetcode/exercises/2.2.3.medium.evaluate_reverse_polish_notation.py
@@ -0,0 +1,45 @@
+# -----------------------
+# Created    : 19/11/2025
+# Last Edited: 19/11/2025 
+# Topics     : 
+# Big O      :
+# Problem Id : 150
+# -----------------------
+
+import math
+
+class Solution:
+    def evalRPN(self, tokens: list[str]) -> int:
+
+        stack = []
+        for i in tokens:
+
+            if i == "-":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(b - a)
+
+            elif i == "+":
+                stack.append(stack.pop() + stack.pop())
+
+            elif i == "*":
+                stack.append(stack.pop() * stack.pop())
+
+            elif i == "/":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(int(b / a))
+
+            else:
+                stack.append(int(i))
+
+        return stack[0]
+
+s = Solution()
+print(s.evalRPN(["4","13","5","/","+"]))
+
+
+                
+                
+
+
