9f06475c95e2fe679f3480d4e7c44b5e88888dfd
morenoblue <morenoblue2000@gmail.com>
2025-11-19 08:43:14 +0000

    add go sol to new ex

---
+++ leetcode/exercises/2.2.3.medium.evaluate_reverse_polish_notation.go
@@ -0,0 +1,45 @@
+// -----------------------
+// Created    : 19/11/2025
+// Last Edited: 19/11/2025 
+// Topics     : 
+// Big O      :
+// Problem Id : 150
+// -----------------------
+
+package main
+
+import (
+    "fmt"
+    "strings"
+    "strconv"
+)
+
+func evalRPN(tokens []string) int {
+    stack := []int{}
+    res := 0
+    for _, v := range tokens {
+        if !strings.Contains("+-/*", v) {
+            i, err := strconv.Atoi(v)
+            if err != nil {
+                panic(err)
+            }
+            stack = append(stack, i)        
+            continue
+        } else if v == "-" {
+            res = stack[len(stack) - 2] - stack[len(stack) - 1]
+        } else if v == "*" {
+            res = stack[len(stack) - 2] * stack[len(stack) - 1]
+        } else if v == "/" {
+            res = stack[len(stack) - 2] / stack[len(stack) - 1]
+        } else {
+            res = stack[len(stack) - 2] + stack[len(stack) - 1]
+        }
+        stack = stack[:len(stack)-2]
+        stack = append(stack, res)
+    }
+    return stack[0]
+}
+
+func main() {
+    fmt.Println(evalRPN([]string{"4","13","5","/","+"}))
+}
